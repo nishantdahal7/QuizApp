4084eaf4013f0b2bc91e046d2fad4958161c8e76
Nishant Dahal <dahalnishant22@gmail.com>
2021-10-03 20:51:17 +0545

    all functions

--- appwindow/mainquiz.py
+++ appwindow/mainquiz.py
@@ -177,3 +177,204 @@ def load_questions():
     button.destroy()
     time_in_sec = 0  # To Stop The Timer and Resume The Application Execution Process From Else Part Of The Timer
 
+
+def completed_session():
+    data_handling()  # To Push All Data To The Database
+    messagebox.showinfo("    Quiz Game",
+                        "Quiz Has Finished\n{} you have scored {} out of 4".format(player_name, game_score))
+    global ok_btn, qualify_mssg, qualify_fail_mssg
+    # To Check If Player Has Passed or Failed The Quiz
+    if (game_score >= qualifying_score):
+        top = Toplevel()
+        top.geometry("1250x780")
+        qualify_mssg_label = Label(top, image=qualify_mssg)
+        qualify_mssg_label.pack()
+        ok_button = Button(top, image=okbtn, cursor="hand2", borderwidth=0, bg="#000000", command=top.destroy)
+        ok_button.place(x=545, y=675)
+    else:
+        top = Toplevel()
+        top.geometry("1250x780")
+        qualify_mssg_label = Label(top, image=qualify_fail_mssg, bg="#4e62d2")
+        qualify_mssg_label.pack()
+        ok_button = Button(top, image=okbtn, cursor="hand2", borderwidth=0, bg="#000000",
+                           command=top.destroy)  # , bg="#436fd2"
+    show_frame(frame4)
+
+
+def data_handling():
+    global player_name
+    # To Check If Player Already Exists in Database If Not Then Insert The Player
+    if (get_player_name(player_name) == None):
+        insert_player(player_name, game_score)
+    # If Player Already Exists Then Update His New Score If it is Greater Than The Previously In Database
+    else:
+        fetched_player = get_player_name(player_name)
+        if (fetched_player[1] < game_score):
+            update_score(player_name, game_score)
+
+
+# To Load Top 3 Players From Database
+def load_highscores(frame):
+    frame.tkraise()
+    c.execute("""SELECT * FROM 'QuizData'
+            ORDER BY score DESC
+            LIMIT 0, 3;""")
+    highscores = c.fetchall()
+
+    const_txt = "NAME" + "\t\t\t\t" + "SCORE"
+    txt_label = Label(frame5, font=("Courier New", 20, "bold"), text=const_txt, pady=25, bg="#f8f8f8")
+    txt_label.place(x=475, y=75)
+
+    disty = 225  # This Variable Increments Accordingly To Place The Name & Score Label
+
+    for x in range(0, 3):
+        curr_player = highscores[x][0]
+        curr_score = highscores[x][1]
+
+        username_to_load_label = Label(frame5, font=("Courier New", 18, "bold"), text=curr_player, pady=10,
+                                       bg="#f8f8f8")
+        username_to_load_label.place(x=475, y=disty)
+
+        userscore_to_load_label = Label(frame5, font=("Courier New", 18, "bold"), text=str(curr_score), pady=10,
+                                        bg="#f8f8f8")
+        userscore_to_load_label.place(x=1000, y=disty)
+
+        disty += 90
+
+
+# To Reinitialize All The Values & Jump To HomePage
+def refresh_session():
+    global player_name, game_score, time_in_sec, input_name
+    questions_attempted = 1
+    input_name.delete(0, 'end')
+    txt_mail.delete(0, 'end')
+    txt_contact.delete(0, 'end')
+    var2.set(' ')
+    game_score = 0
+    time_in_sec = 20
+    show_frame(frame1)
+
+
+# To Add Questions & Options In The Database
+def add_questions():
+    global input_question, input_options, input_correct_option, my_canvas, submit_btn
+    top3 = Toplevel()
+    top3.geometry("1100x650")
+    my_canvas = Canvas(top3)
+    my_canvas.pack(fill="both", expand=True)
+    my_canvas.create_image(0, 0, image=add_questions_bg, anchor="nw")
+    my_canvas.create_text(153, 70, text="Add Question :", font=("Helvetica", 18, "bold"), fill="#ffffff", anchor="nw")
+    input_question = Entry(top3)
+    my_canvas.create_window(150, 100, window=input_question, width=750, anchor="nw")
+    input_question.config(font=("Helvetica", 14), bg="#8319d3", fg="#ffffff", highlightthickness=5,
+                          highlightbackground="#74159d", highlightcolor="#74159d")
+    # Options
+    my_canvas.create_text(153, 180, text="Enter Options :", font=("Helvetica", 18, "bold"), fill="#ffffff", anchor="nw")
+    input_options = Entry(top3)
+    input_options.config(font=("Helvetica", 14), bg="#8319d3", fg="#ffffff", highlightthickness=5,
+                         highlightbackground="#74159d", highlightcolor="#74159d")
+    my_canvas.create_window(150, 210, window=input_options, width=750, anchor="nw")
+    # Guide
+    my_canvas.create_text(153, 255, text="**Enter 4 Options Above", font=("Helvetica", 10, "bold"), fill="#e31313",
+                          anchor="nw")
+    my_canvas.create_text(153, 273, text="**Enter Comma Seperated values", font=("Helvetica", 10, "bold"),
+                          fill="#e31313", anchor="nw")
+    # Correct Answers
+    my_canvas.create_text(153, 330, text="Correct Option :", font=("Helvetica", 18, "bold"), fill="#ffffff",
+                          anchor="nw")
+    input_correct_option = Entry(top3)
+    input_correct_option.config(font=("Helvetica", 14), bg="#8319d3", fg="#ffffff", highlightthickness=5,
+                                highlightbackground="#74159d", highlightcolor="#74159d")
+    my_canvas.create_window(153, 360, window=input_correct_option, width=750, anchor="nw")
+    my_canvas.create_text(153, 405,
+                          text="*ENTER THE CORRECT ANSWER ABOVE(Note: It Must Be Present In The Enter Options Row **CASE SENSITIVE ",
+                          font=("Helvetica", 10, "bold"), fill="#e31313", anchor="nw")
+    # Submit Button
+    submit_btn = Button(my_canvas, image=submit_btn_img, border=0, relief=FLAT, bg="#5f4bd1", cursor="hand2",
+                        command=handle_questions_options)
+    submit_btn_window = my_canvas.create_window(450, 540, anchor="nw", window=submit_btn)
+    # Exit Button
+    form_exit_button = Button(my_canvas, image=form_exit_btn, cursor="hand2", borderwidth=0, bg="#683ed2",
+                              command=top3.destroy)
+    exit_btn_window = my_canvas.create_window(1025, 10, anchor="nw", window=form_exit_button)
+
+
+# To Extract Questions & Options From Entrybox
+def handle_questions_options():
+    global get_question, get_options, get_correct_option, my_canvas, submit_btn, err_var
+    get_question = input_question.get().strip()
+    get_options = input_options.get().split(",")
+    get_correct_option = input_correct_option.get()
+    # To Remove Whitespaces if any from options
+    for option in get_options:
+        formatted_option = option.strip()
+        # print(formatted_option)
+        formatted_options_list.append(formatted_option)
+        # print(formatted_options_list)
+    get_correct_option = get_correct_option.strip()
+
+    # Error Checking Function Call
+    validate_questions_options_data()
+
+
+# Function Which Runs In Loop For Error Checking
+def loop_check():
+    global submit_btn, errors, err_var, get_options, get_correct_option, get_question
+    while (errors):
+        submit_btn.wait_variable(err_var)
+        handle_questions_options()
+
+    # Converting List Consisting Of Options Into String To Push it Into The Database
+    # get_options = str(get_options)
+    get_options = cPickle.dumps(formatted_options_list)
+
+    # Adding Data To Database
+    insert_question_options(get_question, get_options, get_correct_option)
+
+    # Question Added Successfully Message
+    success_mssg_label = Label(my_canvas, text="Question Added Successfully!!!", bg="#5f4bd1", fg="#08ff08",
+                               font=("Helvetica", 30, "bold"), anchor="nw")
+    success_mssg_label.place(x=250, y=460)
+    success_mssg_label.after(5000, success_mssg_label.destroy)
+
+
+# Function To Check For Errors If Present
+def validate_questions_options_data():
+    global get_options, get_correct_option, err_var, errors
+
+    # Function Which Sets Error Value To True & Enters The Loop
+    def error_validator():
+        global errors
+        err_var.set(1)
+        errors = True
+        top2.destroy()
+        loop_check()
+
+    # TO Check if Only 4 Options are Present
+    if (len(get_options) != 4):
+        top2 = Toplevel()
+        top2.geometry("750x325")
+        error_canvas = Canvas(top2)
+        error_canvas.pack(fill="both", expand=True)
+        error_canvas.create_image(50, 60, image=error_img, anchor="nw")
+        error_canvas.create_text(120, 70, text="Please Add Correct Number Of Options As Instructed",
+                                 font=("Helvetica", 18, "bold"), fill="#e31313", anchor="nw")
+        ok_button = Button(error_canvas, image=ok_btn, cursor="hand2", borderwidth=0, command=error_validator)
+        ok_btn_window = error_canvas.create_window(315, 220, anchor="nw", window=ok_button)
+
+    # To Check if Correct Option is Present in Both Rows
+    elif (get_correct_option not in formatted_options_list):
+        top2 = Toplevel()
+        top2.geometry("925x325")
+        error_canvas = Canvas(top2)
+        error_canvas.pack(fill="both", expand=True)
+        error_canvas.create_image(50, 60, image=error_img, anchor="nw")
+        error_canvas.create_text(120, 70, text="Please Check If Correct Option Is Present In Both The Input Rows",
+                                 font=("Helvetica", 18, "bold"), fill="#e31313", anchor="nw")
+        ok_button = Button(error_canvas, image=ok_btn, cursor="hand2", borderwidth=0, command=error_validator)
+        ok_btn_window = error_canvas.create_window(375, 220, anchor="nw", window=ok_button)
+
+    # Setting Errors Value to False And Entering The Loop
+    else:
+        errors = False
+        loop_check()
